f99687c64784e7464d558080b1780c7e9377a85e
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-08 23:47:11 +0700

    setting up POST res

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
 
+app.post("/", function(req, res) {
+    
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.email;
 
+    console.log(firstName, lastName, email);
+})
 
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server is running on port 3000")
 })
