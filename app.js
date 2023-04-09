869a8783ec48eb17d530cc59bba7cf63affd774f
Yayan <111275294+hypryyn@users.noreply.github.com>
2023-04-09 23:43:20 +0700

    mailchimp API setup

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+require("dotenv").config();
+
+console.log(process.env);
 
 const app = express();
 
@@ -12,14 +16,43 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/", function(req, res) {
-    
-    let firstName = req.body.fname;
-    let lastName = req.body.lname;
-    let email = req.body.email;
+app.post("/", function (req, res) {
 
-    console.log(firstName, lastName, email);
-})
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const email = req.body.email;
+
+    let data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+    listId = process.env.List_Id;
+    apiKey = process.env.API_Key;
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/Lists/" + listId;
+    const options = {
+        method: 'POST',
+        auth: "hypryyn:" + apiKey,
+    }
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000")
